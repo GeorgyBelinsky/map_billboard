81a1303bc8de953bd7794b4a0704e797dd341635
GeorgyBelinsky <belinskygeorg@gmail.com>
2024-02-24 13:40:28 +0200

    Changed header of Rentboard request to contain correct authorization token call

--- src/Components/BuyForm/BuyForm.jsx
+++ src/Components/BuyForm/BuyForm.jsx
@@ -36,6 +36,7 @@ const BuyForm = ({ selectedMarkers,setSelectedMarkers, markers, fetchData }) => 
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
+             Authorization: `Bearer ${localStorage.getItem('token')}`,
           },
           body: jsonString,
         });
