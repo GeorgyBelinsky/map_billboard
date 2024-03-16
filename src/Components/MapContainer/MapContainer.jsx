940b4a9e718a7b9215903de541ff6853417f8a89
GeorgyBelinsky <belinskygeorg@gmail.com>
2024-03-16 16:06:03 +0200

    fixed a bug that was opening 2 popups simultaneously, map now zooms into an according billboard

--- src/Components/MapContainer/MapContainer.jsx
+++ src/Components/MapContainer/MapContainer.jsx
@@ -1,104 +1,111 @@
-    import './index.css';
-    import maplibregl from 'maplibre-gl';
-    import { useEffect, useRef, useState } from "react";
-    import { createRoot } from 'react-dom/client';
+import './index.css';
+import maplibregl from 'maplibre-gl';
+import { useEffect, useRef, useState } from "react";
+import { createRoot } from 'react-dom/client';
 
-    import marker_img from '../../assets/pngegg.png';
-    import picture from '../../assets/picture.png';
+import marker_img from '../../assets/pngegg.png';
+import picture from '../../assets/picture.png';
 
-    import PopupContent from '../PopUpContent/PopUpContent';
-    import BuyForm from '../BuyForm/BuyForm';
-    import Actions from '../Actions/Actions';
+import PopupContent from '../PopUpContent/PopUpContent';
+import BuyForm from '../BuyForm/BuyForm';
+import Actions from '../Actions/Actions';
 
-    const MapContainer = ({ markers, fetchData }) => {
-        const mapPlacer = useRef(null);
-        const [selectedMarkers, setSelectedMarkers] = useState([]);
-        const [mapInstance, setMapInstance] = useState(null); // State to store map instance
-        const [markerInstances, setMarkerInstances] = useState([]); // State to store marker instances
-        const [currentPopup, setCurrentPopup] = useState(null); // State to store the currently open popup
+const MapContainer = ({ markers, fetchData }) => {
+    const mapPlacer = useRef(null);
+    const [selectedMarkers, setSelectedMarkers] = useState([]);
+    const [mapInstance, setMapInstance] = useState(null); // State to store map instance
+    const [markerInstances, setMarkerInstances] = useState([]); // State to store marker instances
+    const [currentPopup, setCurrentPopup] = useState(null); // State to store the currently open popup
 
-        useEffect(()=>{
-            console.log(currentPopup);
-        },[currentPopup]);
+    useEffect(() => {
+        const apiKey = 'AAPK44735a5f9a8c49c293f06ef401365ee0iFKx_2d7TjmyQXPR7lHNCLBn5vZRKOIImFBYiTBmupsJuhuLcBmT2ULNUbApHsWV';
+        const basemapStyleURL = `https://basemapstyles-api.arcgis.com/arcgis/rest/services/styles/v2/styles/`;
 
-        useEffect(() => {
-            const apiKey = 'AAPK44735a5f9a8c49c293f06ef401365ee0iFKx_2d7TjmyQXPR7lHNCLBn5vZRKOIImFBYiTBmupsJuhuLcBmT2ULNUbApHsWV';
-            const basemapStyleURL = `https://basemapstyles-api.arcgis.com/arcgis/rest/services/styles/v2/styles/`;
+        const map = new maplibregl.Map({
+            container: mapPlacer.current,
+            style: `${basemapStyleURL}${"arcgis/navigation"}?token=${apiKey}`,
+            center: [50, 50],
+            zoom: 1,
+        });
 
-            const map = new maplibregl.Map({
-                container: mapPlacer.current,
-                style: `${basemapStyleURL}${"arcgis/navigation"}?token=${apiKey}`,
-                center: [50, 50],
-                zoom: 1,
+        // Set the map instance in state
+        setMapInstance(map);
+
+        const markerInstancesArray = markers?.map(marker => {
+            const popup = new maplibregl.Popup();
+            const popupContentNode = document.createElement('div');
+            const reactRoot = createRoot(popupContentNode);
+            reactRoot.render(
+                <PopupContent marker={marker} img={picture} setSelectedMarkers={setSelectedMarkers} />
+            );
+            popup.setDOMContent(popupContentNode);
+
+            const markerInstance = new maplibregl.Marker({ element: customMarker(), })
+                .setLngLat([marker.longitude, marker.latitude])
+                .setPopup(popup)
+                .addTo(map);
+
+            // Listen for popup open event
+            popup.on('open', () => {
+                setCurrentPopup(popup);
             });
 
-            // Set the map instance in state
-            setMapInstance(map);
+            return markerInstance;
+        });
 
-            const markerInstancesArray = markers?.map(marker => {
-                const popup = new maplibregl.Popup();
-                const popupContentNode = document.createElement('div');
-                const reactRoot = createRoot(popupContentNode);
-                reactRoot.render(
-                    <PopupContent marker={marker} img={picture} setSelectedMarkers={setSelectedMarkers} />
-                );
-                popup.setDOMContent(popupContentNode);
+        // Store the marker instances in state
+        setMarkerInstances(markerInstancesArray);
 
-                const markerInstance = new maplibregl.Marker({ element: customMarker(), })
-                    .setLngLat([marker.longitude, marker.latitude])
-                    .setPopup(popup)
-                    .addTo(map);
-                
-                return markerInstance;
-            });
+        return () => map.remove();
+    }, [markers]);
 
-            // Store the marker instances in state
-            setMarkerInstances(markerInstancesArray);
+    const customMarker = () => {
+        const imgMarker = document.createElement('img');
+        imgMarker.src = marker_img;
+        imgMarker.alt = 'Billboard';
+        imgMarker.style.width = '50px';
+        imgMarker.style.height = '50px';
+        return imgMarker;
+    };
 
-            return () => map.remove();
-        }, [markers]);
+    const openPopup = (markerId) => {
+        const marker = markers?.find(marker => marker.billboardId === markerId);
+        if (mapInstance && marker) {
+            // Find the corresponding marker instance
+            const markerInstance = markerInstances.find(instance => instance.getLngLat().toArray().toString() === [marker.longitude, marker.latitude].toString());
 
-        const customMarker = () => {
-            const imgMarker = document.createElement('img');
-            imgMarker.src = marker_img;
-            imgMarker.alt = 'Billboard';
-            imgMarker.style.width = '50px';
-            imgMarker.style.height = '50px';
-            return imgMarker;
-        };
+            if (markerInstance) {
+                const popup = markerInstance.getPopup();
 
-        const openPopup = (markerId) => {
-            const marker = markers?.find(marker => marker.billboardId === markerId);
-            if (mapInstance && marker) {
-                // Find the corresponding marker instance
-                const markerInstance = markerInstances.find(instance => instance.getLngLat().toArray().toString() === [marker.longitude, marker.latitude].toString());
-        
-                if (markerInstance) {
-                    const popup = markerInstance.getPopup();
-        
-                    // Close any previously opened popup
-                    if (currentPopup) {
-                        currentPopup.remove();
-                    }
-        
-                    // If the popup exists, open it
-                    if (popup) {
-                        popup.addTo(mapInstance);
-                        setCurrentPopup(popup); // Set the current popup
-                    }
+                // Close any previously opened popup
+                if (currentPopup) {
+                    currentPopup.remove();
+                }
+
+                // If the popup exists, open it
+                if (popup) {
+                    popup.addTo(mapInstance);
+                    setCurrentPopup(popup); // Set the current popup
+
+                    // Zoom in on the map
+                    mapInstance.flyTo({
+                        center: [marker.longitude, marker.latitude],
+                        zoom: 8, // Set the desired zoom level
+                        essential: true // Animation is considered essential and will not be canceled during user interaction.
+                    });
                 }
             }
-        };
-        
+        }
+    };
 
-        return (
-            <main className="main_container">
-                <div className="mapWrapper" ref={mapPlacer}></div>
-                <BuyForm selectedMarkers={selectedMarkers} setSelectedMarkers={setSelectedMarkers}
-                    markers={markers} fetchData={fetchData} />
-                <Actions markers={markers} openPopup={openPopup} />
-            </main>
-        );
-    }
+    return (
+        <main className="main_container">
+            <div className="mapWrapper" ref={mapPlacer}></div>
+            <BuyForm selectedMarkers={selectedMarkers} setSelectedMarkers={setSelectedMarkers}
+                markers={markers} fetchData={fetchData} />
+            <Actions markers={markers} openPopup={openPopup} />
+        </main>
+    );
+}
 
-    export default MapContainer;
+export default MapContainer;
