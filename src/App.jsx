547f558e15d6326ab19163ac4570f34aeb8f3a62
GeorgyBelinsky <belinskygeorg@gmail.com>
2024-04-15 16:15:23 +0300

    Added User info page route, changed endpoint in accordance to backend

--- src/App.jsx
+++ src/App.jsx
@@ -29,16 +29,6 @@ function App() {
     fetchData();
   }, []);
 
-
-  /*    const markers = [
-      { id: 1, latitude: 49.9935, longitude: 36.2304, adress: 'vul. Sumska 12',
-      rentalDates:["2023.12.28","2023.12.31"], description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' , price:"12312"},
-      //{ id: 2234, latitude: 49.9999, longitude: 36.2000, adress: 'vul. Kultury 20,vul. Kultury 20,vul. Kultury 20,', description: 'ReactDOM.render, and its content is set as the content', price:"235423"  },
-      //{ id: 3, latitude: 50.0200, longitude: 36.2400, adress: 'prosp. Heroiv 4', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', price:"43"},
-      //{ id: 4532, latitude: 50.0300, longitude: 36.2400, adress: 'prosp. Heroiv 4', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.', price:"234" },
-      //{ id: 363463, latitude: 49.9920, longitude: 36.25, adress: 'prosp. Userov 14', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', price:"745" }
-    ];  */
-
   return (
     <>
       <NavBar />
