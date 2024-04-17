6f2ca8c322397dfbe6c5970142ae7cf1f8b32909
GeorgyBelinsky <belinskygeorg@gmail.com>
2024-04-17 16:56:36 +0300

    Image in billboards list now indicates that its clickable and has hover effect

--- src/Components/Actions/ResultElement/ResultElement.jsx
+++ src/Components/Actions/ResultElement/ResultElement.jsx
@@ -1,10 +1,17 @@
 import "./index.css";
 import TEST_INCON from "../../../assets/picture.png";
 
-const ResultElement = ({billboard, openPopup}) => {
+const ResultElement = ({ billboard, openPopup }) => {
     return (
         <div className="result_field">
-            <img className="billboard_image" src={TEST_INCON} onClick={()=>openPopup(billboard?.billboardId)}/>
+
+            <div className="billboard_image">
+                <figure className="image_container">
+                    <img src={TEST_INCON} onClick={() => openPopup(billboard?.billboardId)} />
+                </figure>
+            </div>
+
+
             <div className="general_info">
                 <div className="header_info">
                     <div className="price">{billboard?.dayCost + "$"}</div>
